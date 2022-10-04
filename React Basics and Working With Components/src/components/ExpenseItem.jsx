96dd7ca022acb6150e86a73215cda235a21575d5
redmiRX <redmirx9@gmail.com>
2022-10-04 23:17:40 +0500

    section-3

--- React Basics and Working With Components/src/components/ExpenseItem.jsx
+++ React Basics and Working With Components/src/components/ExpenseItem.jsx
@@ -1,3 +1,11 @@
 export function ExpenseItem() {
-  return <h2>Expense item !</h2>;
+  return (
+    <div>
+      <div>March 21th 2022</div>
+      <div>
+        <h2>Car insurance</h2>
+        <div>294.62</div>
+      </div>
+    </div>
+  );
 }
