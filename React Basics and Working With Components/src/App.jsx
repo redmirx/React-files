993678b09ae7aa9b2226efed6ad26864ebc954f6
redmirx <redmirx9@gmail.com>
2022-10-05 15:52:34 +0500

    section-3

--- React Basics and Working With Components/src/App.jsx
+++ React Basics and Working With Components/src/App.jsx
@@ -1,9 +1,30 @@
 import { ExpenseItem } from "./components/ExpenseItem";
 function App() {
+  const expenses = [
+    {
+      date: new Date(2021, 3, 28),
+      title: "Car Insurance",
+      price: 294.67,
+    },
+    {
+      date: new Date(2022, 7, 3),
+      title: "New desktop computer",
+      price: 6094.67,
+    },
+  ];
   return (
     <div>
       <h1>Hello World</h1>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        date={expenses[0].date}
+        title={expenses[0].title}
+        price={expenses[0].price}
+      ></ExpenseItem>
+      <ExpenseItem
+        date={expenses[1].date}
+        title={expenses[1].title}
+        price={expenses[1].price}
+      ></ExpenseItem>
     </div>
   );
 }
--- React Basics and Working With Components/src/components/ExpenseItem.jsx
+++ React Basics and Working With Components/src/components/ExpenseItem.jsx
@@ -1,12 +1,12 @@
 import "./ExpenseItem.css";
 
-export function ExpenseItem() {
+export function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>March 21th 2022</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car insurance</h2>
-        <div className="expense-item__price">294.62</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.price}</div>
       </div>
     </div>
   );
