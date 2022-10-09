5f216241465d2c89a65a9727a26c744b653d0845
redmirx <redmirx9@gmail.com>
2022-10-09 19:40:16 +0500

    section-5

--- Rendering Lists and Conditional Content/src/App.js
+++ Rendering Lists and Conditional Content/src/App.js
@@ -1,21 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const INITIAL_EXPENSES = [
+  {
+    id: "el-1",
+    title: "Car Insurance",
+    amount: 294.64,
+    date: new Date(2022, 9, 18),
+  },
+  { id: "el-2", title: "New TV", amount: 799.49, date: new Date(2022, 8, 3) },
+];
+
 function App() {
-  const expenses = [
-    {
-      id: "el-1",
-      title: "Car Insurance",
-      amount: 294.64,
-      date: new Date(2022, 9, 18),
-    },
-    { id: "el-2", title: "New TV", amount: 799.49, date: new Date(2022, 8, 3) },
-  ];
+  const [expenses, setExpenses] = useState(INITIAL_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log("In App.js");
-    console.log(expense);
+    // console.log("In App.js");
+    // console.log(expense);
+    setExpenses((prevState) => {
+      return [...prevState, expense];
+    });
   };
   return (
     <div>
