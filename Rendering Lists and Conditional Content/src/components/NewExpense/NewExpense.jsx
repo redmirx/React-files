f242a79aded41b4b33ed95e957b87472e2a947b9
redmirx <redmirx9@gmail.com>
2022-10-12 20:54:11 +0500

    section-5

--- Rendering Lists and Conditional Content/src/components/NewExpense/NewExpense.jsx
+++ Rendering Lists and Conditional Content/src/components/NewExpense/NewExpense.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 export default function NewExpense(props) {
+  const [showForm, setShowForm] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
@@ -10,10 +12,31 @@ export default function NewExpense(props) {
     };
     // console.log(expenseData);
     props.onAddExpense(expenseData);
+    setShowForm(false);
   };
+  console.log(showForm);
+
+  const onStartEditingHandler = () => {
+    setShowForm(true);
+    console.log(showForm);
+  };
+
+  const onStopEditingHandler = () => {
+    setShowForm(false);
+  };
+
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {showForm ? (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={onStopEditingHandler}
+        />
+      ) : (
+        <button onClick={onStartEditingHandler}>Add New Expense</button>
+      )}
+
+      {/* <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} /> */}
     </div>
   );
 }
